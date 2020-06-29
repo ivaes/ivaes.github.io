55c20fa78120ea2de6df206fdda29ef4402410b5
Li <asafonovorg@gmail.com>
2020-06-29 10:35:39 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,79 @@
+// Get the modal
+var modal = document.getElementById("BigPhoto");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//var pic = document.getElementById("myImg");
+
+var modalImg = document.getElementById("modal_pic");
+
+var captionText = document.getElementById("caption");
+
+//pic.onclick = function() {
+//  modal.style.display = "block";
+//  modalImg.src = this.src;
+//}
+
+function goBig2(el) {
+  x = el.getAttribute("data-src");
+  el.setAttribute("src", x);
+
+  el.setAttribute("class", "vert gobig");
+ console.log(el.src);
+}
+
+function goBig(el) {
+  //var mini_pic = HTMLElementObject.src;
+  modal.style.display = "flex";
+  x = el.querySelector('img').getAttribute("data-src");
+  modalImg.setAttribute("src", x);
+  y = el.querySelector('img').getAttribute("alt");
+  captionText.innerHTML = y;
+  document.querySelector('.right_col').classList.add('modal_mode');
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+  document.querySelector('.right_col').classList.remove('modal_mode');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalImg || event.target == modal) {
+    modal.style.display = "none";
+    document.querySelector('.right_col').classList.remove('modal_mode');
+  }
+}
+
+
+
+
+
+
+function sort(z) {
+                      var x = document.getElementsByClassName(z);
+                      console.log(x[0]);
+                      var i;
+                      for (i = 0; i < x.length; i++) { 
+                        x[i].style.opacity = "0";
+                        
+                      }
+                      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
