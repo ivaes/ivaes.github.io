25108ea0610949e5bf276ea0cb64ba2428d098c0
lena <ivaes@yandex.ru>
2020-08-05 11:04:32 +0300

    spass

--- js/designs.js
+++ js/designs.js
@@ -1,4 +1,4 @@
-const photos = [{"name":"BlockBuster. Arcanoid game design.","thumb":"images\/designs\/thumbs\/BlockBuster. Arcanoid game design.jpg","src":"images\/designs\/pics\/BlockBuster. Arcanoid game design.jpg"}].sort(function (a, b) {return 0.5 - Math.random()});
+const photos = [{"name":"BlockBuster. Arcanoid logo and game design.","thumb":"images\/designs\/thumbs\/BlockBuster. Arcanoid logo and game design.jpg","src":"images\/designs\/pics\/BlockBuster. Arcanoid logo and game design.jpg"},{"name":"Spass. Secure passwords creater and keeper. Logo and app design.","thumb":"images\/designs\/thumbs\/Spass. Secure passwords creater and keeper. Logo and app design.jpg","src":"images\/designs\/pics\/Spass. Secure passwords creater and keeper. Logo and app design.jpg"}].sort(function (a, b) {return 0.5 - Math.random()});
 
 for (let i = 0; i < photos.length; ++i) {
   document.write(
